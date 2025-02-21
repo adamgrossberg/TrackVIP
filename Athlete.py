3c177bb3732e3f1ad37aae0e382c7a6422275c70
adamgrossberg <adamgrossberg@gatech.edu>
2025-02-21 14:29:26 -0500

    added name fields

--- Athlete.py
+++ Athlete.py
@@ -1,6 +1,8 @@
 class Athlete:
-    def __init__(self, id: str):
+    def __init__(self, id: str, first_name: str, last_name: str):
         self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
         self.runs = []
     
     def add_run(self, run_id: str):
